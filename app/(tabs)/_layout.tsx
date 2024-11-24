313b1445c97f5072a0042b675ef8a03a57c797b7
Pierre Maurer <91548935+PierreMaurer@users.noreply.github.com>
2024-11-23 18:11:14 -0800

    feat: Init Push of project, HeadBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,59 +1,110 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Pressable, Text } from 'react-native';
+import Svg, { Ellipse } from 'react-native-svg';
 
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
+    name: React.ComponentProps<typeof FontAwesome>['name'];
+    color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+    return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                header: ({ navigation, route, options }) => (
+                    <>
+                        <Link href="/modal" asChild>
+                            <Pressable
+                                style={{
+                                    position: 'absolute',
+                                    top: 80,
+                                    left: 25,
+                                    zIndex: 1,
+                                }}
+                            >
+                                {({ pressed }) => (
+                                    <FontAwesome
+                                        name="gear"
+                                        size={30}
+                                        color={Colors.light.background}
+                                        style={{ opacity: pressed ? 0.5 : 1 }}
+                                    />
+                                )}
+                            </Pressable>
+                        </Link>
+                        <Svg
+                            width="100%"
+                            height="200"
+                            viewBox="0 0 393 143"
+                            style={{ position: 'absolute', top: 0, left: 0, right: 0 }}
+                        >
+                            <Ellipse cx="196" cy="21.5" rx="246" ry="121.5" fill="#D60034" />
+                        </Svg>
+                        <Text
+                            style={{
+                                position: 'absolute',
+                                top: 80,
+                                fontSize: 36,
+                                left: 0,
+                                right: 0,
+                                textAlign: 'center',
+                                transform: [{ translateY: '-50%' }],
+                                color: 'white',
+                                fontWeight: 'bold',
+                                zIndex: 1,
+                            }}
+                        >
+                            Hearth Help
+                        </Text>
+                        <Link href="/modal" asChild>
+                            <Pressable
+                                style={{
+                                    position: 'absolute',
+                                    top: 80,
+                                    right: 25,
+                                    zIndex: 1,
+                                }}
+                            >
+                                {({ pressed }) => (
+                                    <FontAwesome
+                                        name="info-circle"
+                                        size={30}
+                                        color={Colors.light.background}
+                                        style={{ opacity: pressed ? 0.5 : 1 }}
+                                    />
+                                )}
+                            </Pressable>
+                        </Link>
+                    </>
+                ),
+                headerTintColor: 'transparent',
+                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                headerShown: useClientOnlyValue(false, true),
+            }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Tab One',
+                    tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="two"
+                options={{
+                    title: 'Tab Two',
+                    tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+                }}
+            />
+        </Tabs>
+    );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,12 +2,21 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
-
+import {
+  Button,
+  ButtonText,
+  ButtonSpinner,
+  ButtonIcon,
+  ButtonGroup,
+} from "@/components/ui/button"
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Button size="md" variant="solid" action="primary">
+        <ButtonText>Hello World!</ButtonText>
+      </Button>
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
